009d60ed9aa34ce08842c0308ce9a617bc889be8
Viet Devlord <161667269+VincyyStudio@users.noreply.github.com>
2025-06-08 10:43:27 +0700

    Update trangchu.js

--- trangchu.js
+++ trangchu.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function khampha(event) {
     alert("bạn muốn chuyển qua khám phá chứ");
 }
@@ -13,20 +13,3 @@ function hotsearch(event) {
 }
 function login(event) {
     alert("bạn sẽ được chuyển qua trang đăng nhập/đăng kí");
-=======
-function khampha(event) {
-    alert("bạn muốn chuyển qua khám phá chứ");
-}
-function trangchu(event) {
-    alert("bạn đang ở trang chủ")
-}
-function dexuat(event) {
-    alert("bạn muốn chuyển qua đề xuất chứ");
-}
-function hotsearch(event) {
-    alert("bạn muốn chuyển qua hot search chứ -_-");
-}
-function login(event) {
-    alert("bạn sẽ được chuyển qua trang đăng nhập/đăng kí");
->>>>>>> f8b517048bd9d4bf93ef9e5e70b061c9d50ad778
-}
